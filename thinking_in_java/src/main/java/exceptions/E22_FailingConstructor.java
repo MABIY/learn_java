7b9b6e1062b3aa4cb82272f156c15b200d7bca9c
liuhua <liuhua@vipmro.net>
2018-04-26 17:31:08 +0800

    ...

---
+++ thinking_in_java/src/main/java/exceptions/E22_FailingConstructor.java
@@ -0,0 +1,29 @@
+package exceptions;
+
+
+class FailingConstructor {
+    public FailingConstructor(boolean fail)throws ConstructionException {
+        if (fail) {
+            throw new ConstructionException();
+        }
+    }
+}
+
+public class E22_FailingConstructor {
+    public static void main(String[] args) {
+        for (boolean b = false; ; b = !b) {
+            try {
+                System.out.println("Constructing ...");
+                FailingConstructor fc = new FailingConstructor(b);
+                try {
+                    System.out.println("Processing");
+                }finally {
+                    System.out.println("Cleanup...");
+                }
+            } catch (ConstructionException e) {
+                System.out.println("Construction has failed: " + e);
+                break;
+            }
+        }
+    }
+}
