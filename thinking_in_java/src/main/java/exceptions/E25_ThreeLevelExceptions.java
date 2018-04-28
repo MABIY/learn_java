50e8f7e77cc898bbdade430e9b2f48614931feff
liuhua <liuhua@vipmro.net>
2018-04-28 16:44:02 +0800

    ...

---
+++ thinking_in_java/src/main/java/exceptions/E25_ThreeLevelExceptions.java
@@ -0,0 +1,38 @@
+package exceptions;
+
+class Level1Exception extends Exception {}
+
+class Level2Exception extends Level1Exception {}
+
+class Level3Exception extends Level2Exception{}
+
+
+class A {
+    public void f() throws Level1Exception {
+        throw new Level1Exception();
+    }
+}
+
+class B extends A {
+    @Override
+    public void f() throws Level1Exception {
+        throw new Level2Exception();
+    }
+}
+
+class C extends B {
+    @Override
+    public void f() throws Level1Exception {
+        throw new Level3Exception();
+    }
+}
+public class E25_ThreeLevelExceptions {
+    public static void main(String[] args) {
+        A a = new C();
+        try {
+            a.f();
+        } catch (Level1Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
