a7eaa2e2c696f3fc43565e2dd6ddf800d7421398
liuhua <liuhua@vipmro.net>
2018-05-22 11:07:37 +0800

    ..

---
+++ thinking_in_java/src/main/java/initialization/NewVarArgs.java
@@ -0,0 +1,23 @@
+package initialization;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+public class NewVarArgs {
+    static void printArray(Object... args) {
+        for (Object object : args) {
+            System.out.println(object + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Can take individual elements:
+        printArray(new Integer(47), new Float(3.14), new Double(11.11));
+        printArray(47, 3.14F, 11.11);
+        printArray("one","two","three");
+        printArray(new A(),new A(),new A());
+        // Or an array:
+        printArray((Object[]) new Integer[]{1, 2, 3, 4});
+        printArray(); // Empty list is ok
+    }
+}
