a34dd59b39958ba0365c90ebd586d81363668347
liuhua <liuhua@vipmro.net>
2018-07-14 14:50:01 +0800

    ..

--- thinking_in_java/src/main/java/generics1/E22_InstantiateGenericType2.java
+++ thinking_in_java/src/main/java/generics1/E22_InstantiateGenericType2.java
@@ -1,6 +1,7 @@
 package generics1;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 class ClassAsFactory1<T>{
     Class<T> kind;
@@ -15,7 +16,18 @@ class ClassAsFactory1<T>{
             for (Constructor<?> ctor : kind.getConstructors()) {
                 // Look for a constructor with a single parameter:
                 Class<?>[] params = ctor.getParameterTypes();
+                if (params.length == 1) {
+                    if (params[0] == int.class) {
+                        return kind.cast(ctor.newInstance(arg));
+                    }
+                }
             }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
     }
 }
