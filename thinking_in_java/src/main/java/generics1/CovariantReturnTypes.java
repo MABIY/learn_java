ac10e3b89a827df76d1891f994d1936c6de7b8b1
liu hua <jyfc7879@gmail.com>
2018-07-25 21:20:56 +0800

    ..

--- thinking_in_java/src/main/java/generics1/CovariantReturnTypes.java
+++ thinking_in_java/src/main/java/generics1/CovariantReturnTypes.java
@@ -1,10 +1,18 @@
 package generics1;
 
 class Base{}
-class Drived extends Base{}
+class Derived extends Base{}
 
 interface OrdinaryGetter{
     Base get();
 }
+
+interface DerivedGetter extends OrdinaryGetter {
+    // Return type of overrriden method is allowed to vary:
+    Derived get();
+}
 public class CovariantReturnTypes {
+    void test(DerivedGetter d) {
+        Derived d2 = d.get();
+    }
 }
