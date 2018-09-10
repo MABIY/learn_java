857a3ce1bdb7f452b9f05760b7c38d906d8ad9d2
lh <jyfc7879@gmail.com>
2018-09-10 21:58:39 +0800

    ..

---
+++ thinking_in_java/src/main/java/control/Fibonacci.java
@@ -0,0 +1,18 @@
+package control;
+
+public class Fibonacci {
+    int fib(int n) {
+        if(n<2)
+            return 1;
+        return (fib(n - 2) + fib(n - 1));
+    }
+
+    public static void main(String[] args) {
+        Fibonacci f = new Fibonacci();
+        int k = Integer.parseInt("6");
+        System.out.println("First " + k + "Fibonacci number(s): ");
+        for (int i = 0; i < k; i++) {
+            System.out.println(f.fib(i));
+        }
+    }
+}
