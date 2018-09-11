e1665aa95c75f393782a35f42e84129590309fb7
liuhua <liuhua@vipmro.net>
2018-09-11 13:58:49 +0800

    ..

---
+++ thinking_in_java/src/main/java/initialization/ConstructorTest.java
@@ -0,0 +1,11 @@
+package initialization;
+
+class Tester{
+    String s;
+}
+public class ConstructorTest {
+    public static void main(String[] args) {
+        Tester t = new Tester();
+        System.out.println(t.s);
+    }
+}
---
+++ thinking_in_java/src/main/java/initialization/SimpleConstructor.java
@@ -0,0 +1,15 @@
+package initialization;
+//Demonstration of a simple constructor
+
+class Rock{
+    public Rock() { //This is the constructor
+        System.out.println("Rock ");
+    }
+}
+public class SimpleConstructor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Rock();
+        }
+    }
+}
