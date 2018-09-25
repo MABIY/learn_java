f4e782f3f27efce7e14ffda749aff796ea4c2585
liuhua <liuhua@vipmro.net>
2018-09-25 17:42:57 +0800

    ..

---
+++ thinking_in_java/src/main/java/polymorphism/E15_PolyConstructors2.java
@@ -0,0 +1,25 @@
+package polymorphism;
+
+import static access.util.Print.print;
+
+class RectangularGlyph extends Glyph {
+    private int width = 4;
+    private int height = 5;
+
+    public RectangularGlyph(int width, int height) {
+        this.width = width;
+        this.height = height;
+        print("RectangularGlyph.RectangularGlyph(), width = " + width +"" +
+                ", height = " + height);
+    }
+
+    void draw() {
+        print("RectangularGlyph.draw(), area = " + width * height);
+    }
+}
+public class E15_PolyConstructors2 {
+    public static void main(String[] args) {
+        new RoundGlyph(5);
+        new RectangularGlyph(2, 2);
+    }
+}
