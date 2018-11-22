dc5ed431049493b61fe71aedb0b307a3ab4c90fc
liuhua <liuhua@vipmro.net>
2018-11-22 17:13:06 +0800

    ..

---
+++ thinking_in_java/src/main/java/strings/ReplacingStringTokenizer.java
@@ -0,0 +1,26 @@
+package strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * @author: liuHua
+ * @create: 2018-11-22 17:05
+ **/
+
+public class ReplacingStringTokenizer {
+	public static void main(String[] args) {
+		String input = "But I'm not dead yet! I feel happy";
+		StringTokenizer stoke = new StringTokenizer(input);
+		while (stoke.hasMoreElements()) {
+			System.out.print(stoke.nextToken() + " ");
+		}
+		System.out.println();
+		System.out.println(Arrays.toString(input.split(" ")));
+		Scanner scanner = new Scanner(input);
+		while (scanner.hasNext()) {
+			System.out.print(scanner.next() + " ");
+		}
+	}
+}
---
+++ thinking_in_java/src/main/java/strings/SimpleRead.java
@@ -0,0 +1,38 @@
+package strings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * @author: liuHua
+ * @create: 2018-11-22 15:50
+ **/
+
+public class SimpleRead {
+	public static BufferedReader input = new BufferedReader(
+			new StringReader("Sir Robin of Camelot\n22 1.61803")
+	);
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println("What is your name?");
+			String name = input.readLine();
+			System.out.println(name);
+			System.out.println(
+					"How old ary you? What is your favorite double?"
+			);
+			System.out.println("(input: <age> <double>)");
+			String numbers = input.readLine();
+			System.out.println(numbers);
+			String[] numArray = numbers.split(" ");
+			int age = Integer.parseInt(numArray[0]);
+			double favorite = Double.parseDouble(numArray[1]);
+			System.out.printf("Hi %s.\n",name);
+			System.out.printf("In 5 yeare you will be %d.\n",age+5);
+			System.out.printf("My favorite double is %f.", favorite/2);
+		} catch (IOException e) {
+			System.out.println("I/0 exception");
+		}
+	}
+}
