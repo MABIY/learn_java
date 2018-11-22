9d443b744aca1ff15bb2ba5028e30fb11721d487
liuhua <liuhua@vipmro.net>
2018-11-22 19:41:52 +0800

    ..

--- thinking_in_java/src/main/java/strings/ReplacingStringTokenizer.java
+++ thinking_in_java/src/main/java/strings/ReplacingStringTokenizer.java
@@ -14,8 +14,10 @@ public class ReplacingStringTokenizer {
 		String input = "But I'm not dead yet! I feel happy";
 		StringTokenizer stoke = new StringTokenizer(input);
 		while (stoke.hasMoreElements()) {
+		
 			System.out.print(stoke.nextToken() + " ");
 		}
+		
 		System.out.println();
 		System.out.println(Arrays.toString(input.split(" ")));
 		Scanner scanner = new Scanner(input);
