23a1b0f3f9fbfd328fe56997d659d29059332e02
lh <jyfc7879@gmail.com>
2018-12-05 21:28:39 +0800

    ..

--- thinking_in_java/src/main/java/generics/util/ContainerMethodDifferences.java
+++ thinking_in_java/src/main/java/generics/util/ContainerMethodDifferences.java
@@ -1,5 +1,7 @@
 package generics.util;
 
+import sun.reflect.generics.tree.Tree;
+
 import java.lang.reflect.Method;
 import java.util.*;
 
@@ -34,21 +36,35 @@ public class ContainerMethodDifferences {
 		object.add("clone");
 	}
 	
-	static void difference(Class<?> superset, Class<?> subset) {
-		System.out.println(superset.getSimpleName() + " extends " + subset.getSimpleName() + ", adds: ");
+	static void difference(Class<?> subSet, Class<?> supSet) {
+		System.out.println(subSet.getSimpleName() + " extends " + supSet.getSimpleName() + ", adds: ");
 		Set<String> comp = Sets.difference(
-				methodSet(superset), methodSet(subset)
+				methodSet(subSet), methodSet(supSet)
 		);
 		comp.removeAll(object); // Don't show 'Object' methods
 		System.out.println(comp);
-		interfaces(superset);
+		interfaces(subSet);
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Collection: " + methodSet(Collection.class));
 		interfaces(Collection.class);
 		difference(Set.class,Collections.class);
-		//fixme
+		difference(HashSet.class,Set.class);
+		difference(LinkedHashSet.class,HashSet.class);
+		difference(TreeSet.class,Set.class);
+		difference(List.class,Collection.class);
+		difference(TreeSet.class,Set.class);
+		difference(List.class,Collection.class);
+		difference(ArrayList.class,List.class);
+		difference(LinkedList.class,List.class);
+		difference(Queue.class,Collection.class);
+		difference(PriorityQueue.class,Queue.class);
+		System.out.println("map: " + methodSet(Map.class));
+		difference(HashMap.class,Map.class);
+		difference(LinkedHashMap.class,HashMap.class);
+		difference(SortedMap.class,Map.class);
+		difference(TreeMap.class, Map.class);
 	}
 	
 }
