9b8a94c94de666598f49ac70bb9f52295fbd3321
lh <jyfc7879@gmail.com>
2018-12-23 19:45:14 +0800

    ..

---
+++ thinking_in_java/src/main/java/arrays/RandomGeneratorsTest.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import util.GeneratorsTest;
+import util.RandomGenerator;
+
+/**
+ * @author: liuHua
+ * @create: 2018-12-23 19:43
+ **/
+
+public class RandomGeneratorsTest {
+	public static void main(String[] args) {
+		GeneratorsTest.test(RandomGenerator.class);
+	}
+}
