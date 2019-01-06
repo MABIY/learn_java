93f8fc86df68c7b58b6f440fa1df2ce1736ff7f6
lh <jyfc7879@gmail.com>
2019-01-06 16:59:00 +0800

    ..

---
+++ thinking_in_java/src/main/java/containers/E07_CrossInsertion.java
@@ -0,0 +1,39 @@
+package containers;
+
+import util.Countries;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+import static access.util.Print.print;
+
+/**
+ * @author: liuHua
+ * @create: 2019-01-06 16:52
+ **/
+
+public class E07_CrossInsertion {
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<>(Countries.names(10));
+		LinkedList<String> ll = new LinkedList<>(
+				Countries.names(20).subList(10, 20)
+		);
+		for (Iterator<String> it = al.iterator(); it.hasNext();) {
+			print(it.next());
+		}
+		print("****");
+		for (Iterator<String> it = ll.iterator(); it.hasNext(); ) {
+			print(it.next());
+		}
+		print("****");
+		int alindex = 0;
+		for (ListIterator<String> lit2 = ll.listIterator(); lit2.hasNext(); ) {
+			al.add(alindex, lit2.next());
+			alindex += 2;
+		}
+		print("al = "+ al);
+		
+	}
+}
