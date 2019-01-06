c05c19ec333b5e9514f7393f3483ad9dd5872072
lh <jyfc7879@gmail.com>
2019-01-06 16:48:20 +0800

    ..

---
+++ thinking_in_java/src/main/java/containers/Unsupported.java
@@ -0,0 +1,66 @@
+package containers;
+
+import reusing.E;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author: liuHua
+ * @create: 2019-01-06 15:29
+ **/
+
+public class Unsupported {
+	static void test(String msg, List<String> list) {
+		System.out.println("--- " + msg + " ---");
+		Collection<String> c = list;
+		Collection<String> subList = list.subList(1, 8);
+		// Copy of the sublist:
+		Collection<String> c2 = new ArrayList<>(subList);
+		try {
+			c.retainAll(c2);
+		} catch (Exception e) {
+			System.out.println("retainAll(): " + e);
+		}
+		try {
+			c.removeAll(c2);
+		} catch (Exception e) {
+			System.out.println("removeAll(): " + e);
+		}
+		try {
+			c.clear();
+		} catch (Exception e) {
+			System.out.println("clear(): " +e);
+		}
+		try {
+			c.add("X");
+		} catch (Exception e) {
+			System.out.println("add(): " + e);
+		}
+		try {
+			((List<String>) c).addAll(c2);
+		} catch (Exception e) {
+			System.out.println("addAll(): " + e);
+		}
+		try {
+			c.remove("C");
+		} catch (Exception e) {
+			System.out.println("remove(): " + e);
+		}
+		// The List.set() method modifies the value but
+		// doesn't change the size of the data structure:
+		try {
+			list.set(0, "X");
+		} catch (Exception e) {
+			System.out.println("List.set(): " + e);
+		}
+	}
+	
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("A B C D E F G H I G K L".split(" "));
+		test("Modifiable copyd", new ArrayList<>(list));
+		test("Arrays.asList()",list);
+	}
+}
