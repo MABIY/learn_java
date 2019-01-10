9eca0522336688b3520887a1515ee15216fb5ce6
liuhua <liuhua@vipmro.net>
2019-01-10 16:04:47 +0800

    ..

---
+++ thinking_in_java/src/main/java/containers/Groundhog2.java
@@ -0,0 +1,24 @@
+package containers;
+
+/**
+ * @author: liuHua
+ * @create: 2019-01-10 15:59
+ **/
+
+public class Groundhog2 extends Groundhog {
+	
+	public Groundhog2(int number) {
+		super(number);
+	}
+	
+	@Override
+	public int hashCode() {
+		return number;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return obj instanceof Groundhog2 &&
+				(number == ((Groundhog2) obj).number);
+	}
+}
