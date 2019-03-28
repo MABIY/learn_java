970ee806f5ad9317950f6ccda7ec490a3ef6787a
lh <jyfc7879@gmail.com>
2019-03-28 13:52:10 +0800

    ..

---
+++ thinking_in_java/src/main/java/operators/HelloDate.java
@@ -0,0 +1,15 @@
+package operators;
+
+import java.util.Date;
+
+import static net.mindview.util.Print.*;
+
+/**
+ * @author lh
+ **/
+public class HelloDate {
+    public static void main(String[] args) {
+        print("Hello, it's: ");
+        print(new Date());
+    }
+}
---
+++ thinking_in_java/src/main/java/operators/PrintTest.java
@@ -0,0 +1,14 @@
+package operators;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * @author lh
+ * Write a program that uses the "short" and long form of print statement
+ **/
+public class PrintTest {
+    public static void main(String[] args) {
+        print("Hello, from short form.");
+        System.out.println("Hello from long form.");
+    }
+}
