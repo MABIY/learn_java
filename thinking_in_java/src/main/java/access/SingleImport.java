78409697cdc4a03850ebb1c4e451fd1b80d4287a
liuhua <jyfc7879@gmail.com>
2019-04-16 18:39:44 +0800

    ..

---
+++ thinking_in_java/src/main/java/access/SingleImport.java
@@ -0,0 +1,11 @@
+package access;
+
+import java.util.ArrayList;
+/**
+ * @author lh
+ **/
+public class SingleImport {
+    public static void main(String[] args) {
+        ArrayList list = new java.util.ArrayList();
+    }
+}
