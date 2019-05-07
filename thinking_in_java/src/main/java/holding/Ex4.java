74680c7def133f29045b80758b2d5357fd63861c
liuhua <jyfc7879@gmail.com>
2019-05-07 19:05:49 +0800

    ..

---
+++ thinking_in_java/src/main/java/holding/Ex4.java
@@ -0,0 +1,10 @@
+package holding;
+
+/**
+ * @author lh
+ * Create a generator class that produces character names (as String objectsas String objectsas String objectsas String objectsas String objectsas String objectsas String objectsas String objectsas String objects)
+ *
+ **/
+public class Ex4 {
+    //todo
+}
