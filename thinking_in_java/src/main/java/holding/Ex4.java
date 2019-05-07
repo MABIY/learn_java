315a8821e36c745df0ee6ef9fdae5f9acb4cb7a1
liuhua <jyfc7879@gmail.com>
2019-05-07 19:29:42 +0800

    ..

--- thinking_in_java/src/main/java/holding/Ex4.java
+++ thinking_in_java/src/main/java/holding/Ex4.java
@@ -1,10 +1,69 @@
 package holding;
 
+import java.util.*;
+
 /**
  * @author lh
  * Create a generator class that produces character names (as String objectsas String objectsas String objectsas String objectsas String objectsas String objectsas String objectsas String objectsas String objects)
+ * from your favorite movie (you can use Snow White or Star Wars as a fallback) each time
  *
  **/
+class Generator{
+    int key = 0;
+
+    public String next(){
+        switch (key) {
+            default:
+            case 0:
+                key++; return "Snow White";
+            case 1:
+                key++; return "Bashful";
+            case 2:
+                key++;return "Doc";
+            case 3:
+                key++;return "Dopey";
+            case 4:
+                key++;
+                return "Grumpy";
+            case 5:
+                key++;
+                return "Happy";
+            case 6:
+                key++;
+                return "Sleepy";
+            case 7:
+                key = 0;
+                return "Sneezy";
+        }
+    }
+
+    public void fillA(String[] a) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = next();
+        }
+    }
+
+    public Collection fill(Collection<String> c, int n) {
+        for (int i = 0; i < n; i++) {
+            c.add(next());
+        }
+        return c;
+    }
+
+}
 public class Ex4 {
-    //todo
+    public static void main(String[] args) {
+        Generator gen = new Generator();
+        String[] a = new String[10];
+        gen.fillA(a);
+        for (String s : a) {
+            System.out.println(s + ", ");
+        }
+        System.out.println();
+        System.out.println(gen.fill(new ArrayList<>(), 10));
+        System.out.println(gen.fill(new LinkedList<>(),10));
+        System.out.println(gen.fill(new HashSet<>(), 10));
+        System.out.println(gen.fill(new LinkedHashSet<>(), 10));
+        System.out.println(gen.fill(new TreeSet<>(),10));
+    }
 }
