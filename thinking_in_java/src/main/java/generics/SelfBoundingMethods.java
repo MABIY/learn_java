202341e6bfd8f31b8710d720bb817cc827416426
liuhua <jyfc7879@gmail.com>
2019-05-29 17:44:16 +0800

    ..

---
+++ thinking_in_java/src/main/java/generics/SelfBoundingMethods.java
@@ -0,0 +1,16 @@
+package generics;
+
+/**
+ * @author lh
+ **/
+public class SelfBoundingMethods {
+    static <T extends SelfBounded<T>> T f(T arg) {
+        return arg.set(arg).get();
+    }
+
+
+    public static void main(String[] args) {
+        A a = f(new A());
+    }
+}
+
