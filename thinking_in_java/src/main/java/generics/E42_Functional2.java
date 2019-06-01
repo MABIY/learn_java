1d6ae1730e9fa6498e2fa248798d592658861213
liuhua <jyfc7879@gmail.com>
2019-06-01 17:19:35 +0800

    ..

--- thinking_in_java/src/main/java/generics/E42_Functional2.java
+++ thinking_in_java/src/main/java/generics/E42_Functional2.java
@@ -2,7 +2,9 @@ package generics;
 
 /**
  * @author lh
- * lhtd
+ * Create two separate classes,with nothing in common.
+ * Each class should hold a value, and at
+ * least have methods that produce that value
  **/
 public class E42_Functional2 {
 }
