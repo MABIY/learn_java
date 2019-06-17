884e1a7902159970f49dcf3905ae9a6071762807
lh <jyfc8979@gmail.com>
2019-06-18 06:48:00 +0800

    ..

--- thinking_in_java/src/main/java/containers/AssociativeArray.java
+++ thinking_in_java/src/main/java/containers/AssociativeArray.java
@@ -1,8 +1,64 @@
 package containers;
 
+import static net.lh.util.Print.print;
+
 /**
  * @author lh
+ * Associates keys with values
  **/
-public class AssociativeArray {
-    //lhtd
+public class AssociativeArray<K,V> {
+    private Object[][] pairs;
+    private int index;
+
+    public AssociativeArray(int length) {
+        pairs = new Object[length][2];
+    }
+
+    public void put(K key, V value) {
+        if (index >= pairs.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        pairs[index++] = new Object[]{key, value};
+    }
+
+    public V get(K key) {
+        for (int i = 0; i < index; i++) {
+            if (key.equals(pairs[i][0])) {
+                return (V) pairs[i][1];
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < index; i++) {
+            result.append(pairs[i][0].toString());
+            result.append(" : ");
+            result.append(pairs[i][1].toString());
+            if (i < index - 1) {
+                result.append(System.lineSeparator());
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        AssociativeArray<String, String> map = new AssociativeArray<>(6);
+        map.put("key", "blue");
+        map.put("grass", "green");
+        map.put("ocean","dancing");
+        map.put("tree", "tall");
+        map.put("earsh","brown");
+        map.put("sun","warn");
+        try {
+            map.put("extra", "object");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            print("Too many objects!");
+        }
+        print(map);
+        print(map.get("ocen"));
+    }
 }
