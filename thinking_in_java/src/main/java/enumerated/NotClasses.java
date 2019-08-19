7acf8a16dc6a6d8b7ffee9e9a06f56f77f238f99
lh <jyfc8979@gmail.com>
2019-08-19 21:55:30 +0800

    ..

--- thinking_in_java/src/main/java/enumerated/NotClasses.java
+++ thinking_in_java/src/main/java/enumerated/NotClasses.java
@@ -1,9 +1,31 @@
 package enumerated;
 
+import static net.mindview.util.Print.print;
+
 /**
  * @author lh
  * Exec: javap -c LikeClasses
  */
+enum LikeClasses{
+    WINKEN{
+        @Override
+        void behavior() {
+            print("Behavior1");
+        }
+    },BLINKEN{
+        @Override
+        void behavior() {
+            print("Behavior2");
+        }
+    },
+    NOD {
+        @Override
+        void behavior() {
+            print("Behavior3");
+        }
+    };
+    abstract void behavior();
+}
 public class NotClasses {
-    //lhtd
+    //void f1(LikeClasses.WINKEN instance) // Nope
 }
