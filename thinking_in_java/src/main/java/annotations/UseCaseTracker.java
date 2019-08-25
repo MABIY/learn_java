66499b4f9663faeee13f2dbfe0b0752b0953cc0d
lh <jyfc8979@gmail.com>
2019-08-25 21:12:58 +0800

    ..

---
+++ thinking_in_java/src/main/java/annotations/UseCaseTracker.java
@@ -0,0 +1,30 @@
+package annotations;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author lh
+ **/
+public class UseCaseTracker {
+    public static void trackUseCases(List<Integer> useCases, Class<?> cl) {
+        for (Method declaredMethod : cl.getDeclaredMethods()) {
+            UseCase uc = declaredMethod.getAnnotation(UseCase.class);
+            if (uc != null) {
+                System.out.println("Found Use Case: " + uc.id() + " " + uc.description());
+                useCases.remove(new Integer(uc.id()));
+            }
+        }
+        for (Integer useCase : useCases) {
+            System.out.println("Warning: Missing use case-" + useCase);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> useCases = new ArrayList<>();
+        Collections.addAll(useCases, 47, 48, 49, 50);
+        trackUseCases(useCases, PasswordUtils.class);
+    }
+}
