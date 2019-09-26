48d0aade5effa9e5dbf6079e7c69a542f09315e2
liuhua <jyfc7879@gmail.com>
2019-09-26 10:05:28 +0800

    ..

--- thinking_in_java/src/main/java/concurrency/AttemptLocking.java
+++ thinking_in_java/src/main/java/concurrency/AttemptLocking.java
@@ -1,15 +1,60 @@
 package concurrency;
 
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author lh
- * Locks in the concurrent library allow you
+ * Lock in the current library allow you
+ * to given up on trying to acuqire a lock
  */
 public class AttemptLocking {
     private ReentrantLock lock = new ReentrantLock();
     public void untimed() {
         boolean captured = lock.tryLock();
-        //lhnw
+        try {
+            System.out.println("tryLock(): " + captured);
+        }finally {
+            if (captured) {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void timed() {
+        boolean captured = false;
+        try {
+            captured = lock.tryLock(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            System.out.println("tryLock(2,TimeUnit.Seconds): " + captured);
+        }finally {
+            if (captured) {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final AttemptLocking al = new AttemptLocking();
+        al.untimed(); // True -- lock is available
+        al.timed(); // True -- lock is available
+        // Now craete a separate task to grab the lock
+        new Thread(){
+            {
+                setDaemon(true);}
+
+            @Override
+            public void run() {
+                al.lock.lock();
+                System.out.println("acquired");
+            }
+        }.start();
+        TimeUnit.MILLISECONDS.sleep(10); // Give the 2nd task a chance
+        al.untimed(); // False --lock grabbed by task
+        al.timed(); // False -- lock grabbed by task
     }
 }
