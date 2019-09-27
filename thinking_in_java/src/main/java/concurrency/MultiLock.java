fe6849968c2148019f51e685533cc791ea82d98b
liuhua <jyfc7879@gmail.com>
2019-09-27 17:45:09 +0800

    ..

---
+++ thinking_in_java/src/main/java/concurrency/MultiLock.java
@@ -0,0 +1,33 @@
+package concurrency;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * @author lh
+ * One thread can reacquire the same lock
+ */
+public class MultiLock {
+    public synchronized void f1(int count) {
+        if (count-- > 0) {
+            print("f1() calling f2() with count " + count);
+            f2(count);
+        }
+    }
+
+    public synchronized void f2(int count) {
+        if (count-- > 0) {
+            print("f2() calling f1() with count " + count);
+            f1(count);
+        }
+    }
+
+    public static void main(String[] args) {
+        final MultiLock multiLock = new MultiLock();
+        new Thread() {
+            @Override
+            public void run() {
+                multiLock.f1(10);
+            }
+        }.start();
+    }
+}
